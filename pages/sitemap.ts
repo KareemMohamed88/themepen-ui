76ad5bf76c776e8a7b557d9e79f33bc727ef3785
Kareemm2 <132186526+KareemMohamed88@users.noreply.github.com>
2024-04-13 22:50:08 +0200

    fix

--- pages/sitemap.ts
+++ pages/sitemap.ts
@@ -3,22 +3,16 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://acme.com',
+      url: 'https://www.themepen.store/',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 1,
     },
     {
-      url: 'https://acme.com/about',
+      url: 'https://www.themepen.store/terms',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
-    },
-    {
-      url: 'https://acme.com/blog',
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.5,
     },
   ]
 }
